1cfeb4a4a9c2bb98fe7c2228e5d49286a5c9d278
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-08 22:29:19 -0700

    Create Statement.tsx

---
+++ frontend/components/Statement.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Router from 'next/router'
+
+export type StatementProps = {
+ id: number;
+ content: string;
+ coded: boolean;
+ comment: string | null;
+}
+
+const Statement: React.FC<{statement: StatementProps}> = ({ statement }) => {
+ return (
+  <div onClick={() => Router.push('/p/[statementID]', `/p/${statement.id}`)}>
+   <div id='content'>
+    {statement.content}
+   </div>
+   <div id='notes'>
+    <StatementAnnotation comment={statement.comment} />
+   </div>
+  </div>
+ )
+}
+
+function StatementAnnotation(props: { comment: string | null }) {
+ const comments = props.comment;
+ if (comments === null) {
+  return (
+   <>
+    Add ability to add comments.
+   </>
+  )
+ }
+ return (
+  <>
+   {comments}
+  </>
+ )
+}
+
+export type EncodingProps = {
+ parent: number | null;
+ id: number | null;
+ codeUsed: string | null;
+ phrase: string | null;
+ comments: string | null;
+}
+
+// TO DO: Best way to also query and load information about the existing encodings, if any 
+
+export default Statement
