7390c8eb3146c55286d04e75c95673204dc0137b
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-11 17:40:06 -0700

    Update noodle.tsx

--- noodle.tsx
+++ noodle.tsx
@@ -1,14 +1,15 @@
+
+
 //NOTE: this file is not used by anything
 //It's for putting thoughts on how to code parts of the app before I have to make a call about where the code in question needs to go 
 
-const statement = document.querySelector('statement').innerHTML.toString();
+const statement = document.querySelector('.statement').innerHTML;
 
 let markCode: string | null = null; 
 //allow for a fallback that fails to code anything but "code, I guess" with null
 
 //get the dropdown:
-const codeSetter = document.querySelector('select') 
-//TODO: add better identifier so I'm not dicking over anyone who wants to add another dropdown for some reason
+const codeSetter = document.querySelector('.codePicker') 
 
 codeSetter.addEventListener('change', () => {
  let markCode: string = `${this.target.value}`; //set the content
@@ -45,11 +46,58 @@ function addMark(statement: string, highlight: string): string {
  }
 
  let highlightedStatement: string = parts.join('');
+
+ //overwrite the innerHTML with the update:
+ statement = highlightedStatement; 
+
+ //return for sending to backend?
  return highlightedStatement;
 }
 
-//TODO: replace innerHTML of statement with highlightedStatement
 //TODO: replace statement in Statements with newest version of highlightedStatement
 //TODO: other database updates
 
 //I think that may have to happen elsewhere than this part of the code?
+
+
+function CodeButton(highlight: string, statement: string, statementID: number, markCode: string | null) {
+ //overwrite displayed content
+ addMark(statement, highlight);
+
+ //TODO: send that back to the Statements database
+ //TODO: push new entry to the Encoding database
+
+}
+
+
+const exampleCodeList:(string[])[] = [['misspelling', 'Misspelling', 'IMAGINE A HEX CODE HERE'], ['age-based', 'Age judgment', 'IMAGINE A HEX CODE HERE'], ['at-me', 'Statement about me', 'IMAGINE A HEX CODE HERE']]
+//this way I can have longer elaborations, and also maybe add hex codes to that list natively?
+
+//this should allow for importing a list of codes 
+const CodePicker = (codeList:(string[])[] ) => {
+// const [code, setCode] = useState('');
+// Do I need that?
+
+ let selectList:string[] = []
+ for (let i = 0; i < codeList.length; i++) {
+  let entry:string[] = codeList[i];
+  let opt: string = '<option value="' + entry[0] + '">' + entry[1] + '</option>'
+  selectList.push(opt)
+ }
+
+ const options = selectList.join('');
+
+ return (
+  <div className='coder'>
+   <label for='codePicker'>Select code:</label>
+   <select 
+    name='codePicker' id='codePicker'
+//    onChange={e => setCode(e.target.value)} 
+// do I need that?
+   >
+    {options}
+   </select>
+   <button onClick={CodeButton}>Code</button>
+  </div>
+ ) //How to make sure that the button is 
+}
