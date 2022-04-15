c93fc200e973e6ec61371baf1d006b276d1d4c21
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-15 06:10:40 -0700

    Create newcode.ts

---
+++ backend/src/routeHaving/maintenance/newcode.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+import express from 'express'
+import { Router } from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+const router = Router()
+
+app.use(express.json())
+app.use(cors())
+
+console.log("newcode is here")
