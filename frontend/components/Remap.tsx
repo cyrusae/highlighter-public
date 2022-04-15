d896ee165f215dd9fa193352e1b227c7caae9a14
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-15 06:12:02 -0700

    Sassmap generator

---
+++ frontend/components/Remap.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Router from 'next/router'
+import { GetServerSideProps } from 'next'
+import { CodeList } from './Glossary'
+
+type Props = {
+ glossary: CodeList[];
+}
+
+const Map: React.FC<Props> = props => {
+ return (
+  <div>
+   <code>
+    (
+    {props.glossary.map(code => `"${code.shortCode}": ${code.colorCode}`, )}
+    );
+   </code>
+   <p><i>(Trim the trailing space and comma off of your output before using it!)</i></p>
+  </div>
+ )
+}
+//TODO: Figuring out how best to let this update more than once without redirecting on the page 
+
+export const getServerSideProps: GetServerSideProps = async () => {
+ const res = await fetch(`https://localhost:3001/gloss`);
+ console.log("here's await res clone text output:"); console.log(await res.clone().text());
+ const glossary = await res.json();
+ console.log("here's the glossary output:"); console.log(glossary);
+ return {
+  props:
+   { glossary }
+ }
+}
+
+export default Map 
