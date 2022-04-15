e30b847537451a49d6562562425c9d33e1c33d99
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-15 06:11:33 -0700

    Route for getting known codes

---
+++ backend/src/routeHaving/maintenance/codegloss.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+import express from 'express'
+import { Router } from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+const router = Router()
+
+app.use(express.json())
+app.use(cors())
+
+console.log("gloss is here")
+
+router.get('/', async (req, res, next) => {
+ const codes = await prisma.codes.findMany();
+ res.json(codes)
+})
+
+export default router 
