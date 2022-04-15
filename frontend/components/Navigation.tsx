9812dfacad1d712715137b15e588d11415055746
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-15 04:51:12 -0700

    Add flag and pause buttons

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -11,28 +11,52 @@ export type NavButtonProps = {
  action: 'next' | 'prev' | 'flag' | 'pause';
 }
 
-const NavButton: React.FC<{current: NavInfoProps, button: string}> = ({current, button}) => {
+const NavButton: React.FC<{current: NavInfoProps, act: NavButtonProps["action"]}> = ({current, act}) => {
  const currentID = current.statementID;
- const act = button;
  //add:
  //&now=${lastUpdated}
  //to the url once date handling for Prisma in SQLite is fixed or I find a worthwhile workaround.
 // const url = "'/" + act;
 
  return (
-  <button onClick={() => {
-   Router.push('/go', `/go?act=${act}&current=${currentID}`)}}>{act}</button>
+  <div aria-role='button' className='button' onClick={() => {
+   Router.push('/go', `/go?act=${act}&current=${currentID}`)}}>
+     {act}
+  </div>
+  )
+}
+
+const FlagButton: React.FC<{current: NavInfoProps, act: NavButtonProps["action"]}> = ({current, act}) => {
+  const newFlag = current.statementID + ' ' + `&#013; &#010;`; //last part should produce line breaks
+
+  //changed from button to div to avoid automatic onsubmit events in case that would be a risk
+  return (
+    <div aria-role='button' className='button' onClick={() => {
+      if (document.getElementById('flags').innerText.includes(current.statementID.toString()) === false)
+        { document.getElementById('flags').innerHTML += newFlag }
+    }}>{act}</div>
+  )
+}
+
+const PauseButton: React.FC<{current: NavInfoProps, act: NavButtonProps["action"]}> = ({current, act}) => {
+  const currentID = current.statementID;
+  //TODO: add save on click 
+  //TODO: determine how best to handle saving
+  //NOTE: may end up being cookies
+  return (
+    <div aria-role='button' className='button'>
+      {act}
+    </div>
   )
 }
 
 export const Nav: React.FC<{current: NavInfoProps}> = ({current}) => {
- const next = 'next';
- const prev = 'prev';
-
  return (
   <div id='nav'>
-   <NavButton current={current} button={prev} />
-   <NavButton current={current} button={next} />
+   <NavButton current={current} act={'prev'} />
+   <PauseButton current={current} act={'pause'} />
+   <FlagButton current={current} act={'flag'} />
+   <NavButton current={current} act={'next'} />
   </div>
  )
 }
