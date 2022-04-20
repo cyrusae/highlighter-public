affa38e0e8be064e79f12b17f53a6b476039d030
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-20 16:56:00 -0700

    Add Bootstrap

--- frontend/pages/codes.tsx
+++ frontend/pages/codes.tsx
@@ -1,20 +1,36 @@
 import { GetServerSideProps } from 'next'
-import React from 'react'
+import React, { useState } from 'react'
 import Colormaker, { CodeProps } from '../components/Colormaker'
 import Glossary, { CodeList } from '../components/Glossary'
 import Map from '../components/Remap'
+import { Row, Col, Container, Stack } from 'react-bootstrap'
+
 
 type Props = {
  initialGlossary: CodeProps[];
 }
 
 const Codepage: React.FC<Props> = (props) => {
+ useState();
  return (
-  <div id='codepage'>
-   <Glossary glossary={props.initialGlossary} />
-   <Colormaker/>
-   <Map glossary={props.initialGlossary}/>
-  </div>
+  <Container fluid="md" id='codepage'>
+  <Row>
+   <Col>
+    <Glossary glossary={props.initialGlossary} />
+   </Col>
+  </Row>
+    <Colormaker glossary={props.initialGlossary}/>
+   <Row>
+    <Col>
+     <Glossary glossary={props.initialGlossary} />
+    </Col>
+   </Row>
+   <Row>
+    <Col>
+    <Map glossary={props.initialGlossary}/>
+    </Col>
+   </Row>
+  </Container>
  )
 }
 export default Codepage
@@ -22,9 +38,9 @@ export default Codepage
 
 export const getServerSideProps: GetServerSideProps = async () => {
  const res = await fetch(`http://localhost:3001/gloss`);
- console.log("here's await res clone text output:"); console.log(await res.clone().text());
+// console.log("here's await res clone text output:"); console.log(await res.clone().text());
  const initialGlossary = await res.json();
- console.log("here's the glossary output on load:"); console.log(initialGlossary);
+// console.log("here's the glossary output on load:"); console.log(initialGlossary);
  return {
   props:
    { initialGlossary }
