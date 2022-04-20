d1134fc0948755b61b0c32930c3a0cf19bda7832
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-20 00:10:31 -0700

    Router cleanup

--- backend/src/routeHaving/maintenance/newcode.ts
+++ backend/src/routeHaving/maintenance/newcode.ts
@@ -8,8 +8,14 @@ const app = express()
 
 const router = Router()
 
+const corsOptions ={
+ origin:'http://localhost:3000', 
+ credentials:true,            //access-control-allow-credentials:true
+ optionSuccessStatus:200
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 console.log("newcode is here")
 
