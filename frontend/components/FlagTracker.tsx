326bb1756e0c4d573a2ade9bb8fc74d90e0b8f77
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-20 19:50:40 -0700

    Update FlagTracker.tsx
    #42

--- frontend/components/FlagTracker.tsx
+++ frontend/components/FlagTracker.tsx
@@ -1,16 +1,67 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import Button from 'react-bootstrap/Button'
-import Offcanvas from 'react-bootstrap/Offcanvas'
+import { Col, Offcanvas, Button, Stack } from 'react-bootstrap'
 
 export const FlagBar: React.FC<{}> = () => {
+ const [show, setShow] = useState(false);
+ const handleClose = () => setShow(false);
+ const handleOpen = () => setShow(true);
  return (
-  <div id='flagbar' />
+  <>
+   <div id='flagS' onClick={handleOpen}>⚑</div>
+   <Offcanvas show={show} onHide={handleClose}>
+    <Offcanvas.Header closeButton>
+     This session
+    </Offcanvas.Header>
+    <Offcanvas.Body>
+     <Flags />
+    </Offcanvas.Body>
+   </Offcanvas>
+  </>
  )
 }
 
+const Flags: React.FC<{}> = () => {
+ // console.log(current);
+
+//  useEffect(() => {
+//   const bar = document.getElementById('flagbar')
+//   if (localStorage.getItem('flag') === null) {
+//    bar.style.backgroundColor = "#53565A";
+//   } else {
+//    bar.style.backgroundColor = "#610C04"/
+//   }
+//  })
+
+  const [ flagged, setFlagged ] = useState('');
+  useEffect(() => {
+   let flags = localStorage.getItem('flag');
+   if (flags !== null) setFlagged(localStorage.getItem('flag')); 
+   if (flags === null) flags = '';
+  })
+ 
+  return (
+   <div className='flagged'>
+    Statements flagged this session:
+     <ul dangerouslySetInnerHTML={{__html: flagged}}></ul>
+     <Button size="sm" type='button' onClick={() => axios.post('http://localhost:3001/saveflags', {
+      content: flagged,
+      type: 'flags'
+     })}>
+      Save flags
+     </Button>
+     <Button size="sm" type='button' onClick={() => navigator.clipboard.writeText(flagged)}>
+      Copy flags
+     </Button>
+     <Button size="sm" type='button' onClick={() => {localStorage.clear(); setFlagged('')}}>
+      Clear flags
+     </Button>
+   </div>
+  )
+ }
+
 const FlagTracker: React.FC<{current: number}> = ({current}) => {
- console.log(current);
+// console.log(current);
  const [ flagged, setFlagged ] = useState('')
 
  useEffect(() => {
