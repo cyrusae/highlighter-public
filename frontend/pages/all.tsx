322a3cc43e52862e382b75583946d6de6bcd185e
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-24 03:44:22 -0700

    Create all.tsx

---
+++ frontend/pages/all.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import Statement, { StatementProps } from '../components/Statement'
+import Card from 'react-bootstrap/Card'
+import Multiview from '../components/Multiview'
+
+type Props = {
+  pile: StatementProps[]
+}
+
+const Desk: React.FC<Props> = props => {
+  return (
+    <Multiview>
+     {props.pile.map(statement => (
+      <Card body key={statement.statementID} className="statement">
+       <Statement statement={statement} />
+     </Card>
+    ))}
+  </Multiview>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch('http://localhost:3001/all') 
+//  console.log(await res.clone().text()) 
+  const pile = await res.json()
+  console.log(pile) 
+  return {
+   props: 
+    { pile }
+  }
+}
+
+export default Desk
