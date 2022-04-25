a7e3cb0d00cb3c867c6910ae481fdffbc3c16b5b
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-24 20:05:09 -0700

    Some problems with routing

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "60061f9550507d47"; }
+/******/ 		__webpack_require__.h = function() { return "a8de06511f060d07"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
