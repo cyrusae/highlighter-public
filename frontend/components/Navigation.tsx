fe9618c703c2009c3ab5501c765a712d1b3aab58
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-28 04:48:22 -0700

    Update Navigation.tsx

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,16 +1,39 @@
-import React, { PureComponent } from 'react'
+import React, { useEffect, useState } from 'react'
 import Router from 'next/router'
 import axios from 'axios'
 import { ReaderProps } from '../pages/s/[statementID]'
-import { StatementProps } from './Statement'
 import Button from 'react-bootstrap/Button'
 import Navbar from 'react-bootstrap/Navbar'
 import Stack from 'react-bootstrap/Stack'
-
+import { Col, ButtonGroup, ToggleButtonGroup } from 'react-bootstrap'
 
 export type NavButtonProps = {
  action: 'next' | 'prev' | 'flag' | 'pause';
 	read: ReaderProps[];
+}
+
+const NavTogl: React.FC<{from: ReaderProps}> = ({ from }) => {
+	const currentID = from.statement["statementID"];
+	const next = from.nextID;
+	const prev = from.prevID;
+	return (
+		<>
+		<ButtonGroup>
+			<Button size='lg' id='nextButton' type='button' className='ton' onClick={() => {
+					const now = Date.now();
+    	axios.put('http://localhost:3001/leave/',	{
+						currentID: currentID,
+						now: now,
+					});
+     Router.push('/s/', `/s/${next}`)}}>
+
+			</Button>
+			<Button>
+
+			</Button>
+		</ButtonGroup>
+		</>
+	)
 }
 
 const NavButton: React.FC<{from: ReaderProps; act: NavButtonProps["action"]}> = ({from, act}) => {
@@ -24,7 +47,7 @@ const NavButton: React.FC<{from: ReaderProps; act: NavButtonProps["action"]}> = 
 
  if (act === 'next') {
   return (
-   <Button id='nextButton' type='button' className='button' onClick={() => {
+   <Button size='lg' id='nextButton' type='button' className='button' onClick={() => {
 					const now = Date.now();
     	axios.put('http://localhost:3001/leave/', { 
 						currentID: current,
@@ -35,14 +58,14 @@ const NavButton: React.FC<{from: ReaderProps; act: NavButtonProps["action"]}> = 
    </Button>
   )} else if (act === 'prev') {
     return (
-     <Button id='prevButton' type='button' className='button' onClick={() => {
+     <Button size='lg' id='prevButton' type='button' className='button' onClick={() => {
       axios.put('http://localhost:3001/leave/', { currentID: current });
       Router.push('/s/', `/s/${prev}`)}}>
        {act}
  		</Button>
  	)} else if (act === 'flag') {
    	return (
-      <Button variant="danger" id='flagButton' type='button' className='button' onClick={() => {
+      <Button size='sm' disabled variant="danger" id='flagButton' type='button' className='button' onClick={() => {
        const now = Date.now();
   				 axios.put('http://localhost:3001/leave/', { 
 								currentID: current,
@@ -53,7 +76,7 @@ const NavButton: React.FC<{from: ReaderProps; act: NavButtonProps["action"]}> = 
 						</Button>
  	)} else {
 				return (
-   	 <Button 
+   	 <Button size='sm'
      id='pauseButton' type='button' className='button' 
 					onClick={() => {
 						const now = Date.now();
@@ -68,11 +91,34 @@ const NavButton: React.FC<{from: ReaderProps; act: NavButtonProps["action"]}> = 
 }
 
 export const Nav: React.FC<{current: ReaderProps}> = ({current}) => {
+
+	const [ flagged, setFlagged ] = useState('');
+	useEffect(() => {
+		const bg = document.getElementById('flagS');
+  let flags = localStorage.getItem('flag');
+		bg.style.backgroundColor = '#53565A';
+		if (flags !== null) {
+			setFlagged(localStorage.getItem('flag'));
+			bg.style.backgroundColor = '#610C04';
+		}; 
+  if (flags === null) {
+			flags = '';
+	  document.getElementById('flagButton').addEventListener('click', function flag() {
+  	 if (flags.includes(current.toString()) === false) {
+  	  const commentary = prompt('Why flag this?', '');
+  	  flags += ('<li>' + current + ': ' + commentary + '</li>');
+					return;
+ 	  };
+ 	  localStorage.setItem('flag', flags);
+ 	  setFlagged(localStorage.getItem('flag'));
+				bg.style.backgroundColor = '#53565A';
+ 	 });
+		}
+ })
  return (
-  <Stack gap={2} id='nav'>
+  <Stack gap={1} id='nav'>
    <NavButton from={current} act={'prev'} />
-   <NavButton from={current} act={'pause'} />
-   <NavButton from={current} act={'flag'} />
+			<NavButton from={current} act={'pause'} /><NavButton from={current} act={'flag'} />
    <NavButton from={current} act={'next'} />
   </Stack>
  )
