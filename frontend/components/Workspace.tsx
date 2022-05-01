69a7bcf5056aad36aa8fa5ee6cc804347d4ebe04
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-30 21:41:02 -0700

    Single statements are now Cards

--- frontend/components/Workspace.tsx
+++ frontend/components/Workspace.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, useEffect } from "react"
+import Card from 'react-bootstrap/Card'
 import { CodeList } from "./Glossary";
 import { StatementProps } from "./Statement"
 
@@ -9,11 +10,24 @@ const StatementBox: React.FC<{sample: StatementProps[], glossary: CodeList[]}> =
  let statementID = sample["statementID"];
 
  return (
- <div className='statement' id='statement'>
-  <div className='metadata'>ID: {statementID}</div>
-  <div className='statebox' id='statebox' dangerouslySetInnerHTML={{__html: content}}/>
- </div>
+  <Card>
+   <Card.Header>
+    ID: {statementID}
+   </Card.Header>
+   <Card.Text>
+    <div className="statext" dangerouslySetInnerHTML={{__html: content}}/>
+   </Card.Text>
+   <Card.Footer>
+    
+   </Card.Footer>
+  </Card>
 )
 }
 
 export default StatementBox
+
+//previously:
+//<div className='statement' id='statement'>
+//<div className='metadata'>ID: {statementID}</div>
+//<div className='statebox' id='statebox' dangerouslySetInnerHTML={{__html: content}}/>
+//</div>
