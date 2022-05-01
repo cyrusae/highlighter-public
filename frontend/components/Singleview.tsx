6d5fe2cf759ee53f230ddb9668c636e446650644
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-04-30 21:49:46 -0700

    Update Singleview.tsx

--- frontend/components/Singleview.tsx
+++ frontend/components/Singleview.tsx
@@ -5,9 +5,9 @@ import Layout from './Layout'
 const Singleview: React.FC<{}> = (props) => {
  return (
   <Layout>
-   <Container fluid="md" className="singleview">
-    {props.children}
-   </Container>
+   {props.children}
   </Layout>
  )
 }
+
+export default Singleview
