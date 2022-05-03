3cbafc4104dff93c86b169075790788b6b8e5e88
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-05-03 16:58:57 -0700

    Singleview experiments

--- frontend/components/Singleview.tsx
+++ frontend/components/Singleview.tsx
@@ -1,6 +1,7 @@
 import react from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
 import Layout from './Layout'
+import FootBook from './Glossary'
 
 const Singleview: React.FC<{}> = (props) => {
  return (
