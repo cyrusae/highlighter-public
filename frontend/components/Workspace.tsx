85f5263de61b555f8fa32a2c737a68142dc3c88a
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-05-03 18:03:14 -0700

    Add paragraph creation

--- frontend/components/Workspace.tsx
+++ frontend/components/Workspace.tsx
@@ -6,19 +6,29 @@ import { StatementProps } from "./Statement"
 // import styles from './Statement.module.css'
 
 const StatementBox: React.FC<{sample: StatementProps[], glossary: CodeList[]}> = ( {sample, glossary} ) => {
- let content = sample["content"];
+ let content = sample["content"].trim();
  let statementID = sample["statementID"];
+ const pOpen = `<p>`
+ const pClos = `</p>`
+ const p1azy = `<p/>`
+ const pLa2y = `<p />`
+ const start = content.slice(0, (pOpen.length - 1));
+ const stop = content.slice((content.length - pClos.length - 1), (content.length - 1));
+ if (start != pOpen && stop != pClos) {
+  let paragraphs = pOpen + content + pClos;
+  paragraphs.replaceAll((p1azy || pLa2y), (pClos + pOpen))
+  content = paragraphs;
+ }
 
  return (
   <Card>
    <Card.Header>
     ID: {statementID}
    </Card.Header>
-   <Card.Text>
+   <Card.Body>
     <div id='statement' className="statext" dangerouslySetInnerHTML={{__html: content}}/>
-   </Card.Text>
+   </Card.Body>
    <Card.Footer>
-    
    </Card.Footer>
   </Card>
 )
