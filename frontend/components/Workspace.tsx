e8b4e2fbb23cc03ece7de0915f9683977e076604
Cyrus <86699936+cyrusae@users.noreply.github.com>
2022-05-11 20:51:38 -0700

    Update Workspace.tsx

--- frontend/components/Workspace.tsx
+++ frontend/components/Workspace.tsx
@@ -9,23 +9,23 @@ import { Button } from "react-bootstrap";
 const StatementBox: React.FC<{sample: StatementProps[], glossary: CodeList[]}> = ( {sample, glossary} ) => {
  let content = sample["content"].trim();
  let statementID = sample["statementID"];
- const pOpen = `<p>`
- const pClos = `</p>`
- const p1azy = `<p/>`
- const pLa2y = `<p />`
- const start = content.slice(0, (pOpen.length - 1));
- const stop = content.slice((content.length - pClos.length - 1), (content.length - 1));
- if (start !== pOpen && stop !== pClos) {
+ const pOpen: string = `<p>`
+ const pClos: string = `</p>`
+ const p1azy: string = `<p/>`
+ const pLa2y: string = `<p />`
+ const start: string = content.slice(0, (pOpen.length - 1));
+ const stop: string = content.slice((- pClos.length));
+ if (start != pOpen && stop != pClos) {
   let paragraphs = pOpen + content + pClos;
-  paragraphs.replaceAll((p1azy || pLa2y), (pClos + pOpen))
+  paragraphs.replaceAll((p1azy), (pClos + pOpen));   paragraphs.replaceAll((pLa2y), (pClos + pOpen));
   content = paragraphs;
  }
- const [ button, setButton ] = useState('outline-danger')
+ const [ button, setButton ] = useState('outline-danger');
  function checkStatus() {
   {
    if (typeof document !== undefined) {
     const flags = localStorage.getItem('flag');
-    if (!(!flags) && flags !== '') {
+    if (!(!flags) && flags != '') {
      setButton('danger')
     } else {
      setButton('outline-danger')
